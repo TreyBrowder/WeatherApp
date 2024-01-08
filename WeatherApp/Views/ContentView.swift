f3d9547a0e9d25173a3d9c1776e0da2d7053a219
Treybrowder <tbrowder7@gmail.com>
2024-01-08 09:46:09 -0500

    moved header into its own UI view

--- WeatherApp/Views/ContentView.swift
+++ WeatherApp/Views/ContentView.swift
@@ -23,48 +23,6 @@ struct ContentView: View {
     }
 }
 
-struct HeaderView: View {
-    
-    var viewModel = WeatherViewModel()
-    
-    var body: some View {
-        VStack(spacing: 0) {
-            Text(viewModel.headerViewModel.location)
-                .bold()
-                .foregroundColor(.white)
-                .font(.system(size: 36))
-            
-            Image(systemName: "sunset.fill")
-                .renderingMode(.original)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 190, height: 190, alignment: .center)
-            
-            Text(viewModel.headerViewModel.currentTemp)
-                .bold()
-                .foregroundColor(.white)
-                .font(.system(size: 95))
-            
-            Text(viewModel.headerViewModel.currentConditions)
-                .bold()
-                .foregroundColor(.white)
-                .font(.system(size: 25))
-            
-            HStack {
-                Text("H:76°")
-                    .bold()
-                    .foregroundColor(.white)
-                    .font(.system(size: 20))
-                
-                Text("L:58°")
-                    .bold()
-                    .foregroundColor(.white)
-                    .font(.system(size: 20))
-            }
-        }
-    }
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ WeatherApp/Views/HeaderView.swift
@@ -0,0 +1,60 @@
+//
+//  HeaderView.swift
+//  WeatherApp
+//
+//  Created by Trey Browder on 1/7/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct HeaderView: View {
+    
+    var viewModel = WeatherViewModel()
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Text(viewModel.headerViewModel.location)
+                .bold()
+                .foregroundColor(.white)
+                .font(.system(size: 36))
+            
+            Image(systemName: "sunset.fill")
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 190, height: 190, alignment: .center)
+            
+            Text(viewModel.headerViewModel.currentTemp)
+                .bold()
+                .foregroundColor(.white)
+                .font(.system(size: 95))
+            
+            Text(viewModel.headerViewModel.currentConditions)
+                .bold()
+                .foregroundColor(.white)
+                .font(.system(size: 25))
+            
+            HStack {
+                Text("H:76°")
+                    .bold()
+                    .foregroundColor(.white)
+                    .font(.system(size: 20))
+                
+                Text("L:58°")
+                    .bold()
+                    .foregroundColor(.white)
+                    .font(.system(size: 20))
+            }
+        }
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+            .preferredColorScheme(.dark)
+            
+    }
+}
+
